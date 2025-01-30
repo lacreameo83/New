7395b27c68d6cb5935c2ecebeb7c58bca2bbda4b
precious osonwa <142677806+lacreameo83@users.noreply.github.com>
2025-01-30 11:19:43 +0100

    added the main section

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Herosection from "./_components/mainpages/Herosection";
+import MainSection from "./_components/mainpages/Mainsection";
 
 export default function Home() {
   return (
     <div>
       <Herosection />
+      <MainSection />
     </div>
   );
 }
